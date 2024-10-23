2606208cf3dfea8c677e698ed88a54f2b5e22622
biju joseph <biju.joseph@gds.ey.com>
2024-10-23 03:45:38 -0500

    new check in

---
+++ modules/bootcamp/ice_cream_api/transformations/create_asset_hierarchy.sql
@@ -0,0 +1,19 @@
+/* MAPPING_MODE_ENABLED: false */
+/* {"version":1,"sourceType":"raw","mappings":[{"from":"","to":"externalId","asType":"STRING"},{"from":"","to":"parentExternalId","asType":"STRING"},{"from":"","to":"source","asType":"STRING"},{"from":"","to":"name","asType":"STRING"},{"from":"","to":"description","asType":"STRING"},{"from":"","to":"metadata","asType":"MAP<STRING, STRING>"},{"from":"","to":"dataSetId","asType":"BIGINT"},{"from":"","to":"labels","asType":"ARRAY<STRING>"}]} */
+
+SELECT
+  external_id AS externalId,
+  IF(
+    parent_external_id = ''
+    OR parent_external_id IS NULL,
+    '',
+    parent_external_id
+  ) AS parentExternalId,
+  name AS name,
+  json_tuple(metadata,'site', 'type', 'cycle time', 'Function', 'src', 'Machine type') AS (site, type, cycle_time, function, src, machine_type),
+  to_metadata(site, type, cycle_time, function, src, machine_type) AS metadata,
+  description AS description,
+  'from RAW' AS source,
+  Dataset_id('ds_icapi') AS dataSetId
+FROM
+  `ice_cream_api`.icapi_assets_extractor
